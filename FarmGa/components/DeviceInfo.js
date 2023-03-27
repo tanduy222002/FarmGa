77d57faf4d91f07ef5a7b6451978b2810f4de2b7
The Roamer <dangkhoa29012002@gmail.com>
2023-03-27 10:11:17 +0700

    add control device page UI

---
+++ FarmGa/components/DeviceInfo.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { View, Text , StyleSheet } from 'react-native'
+import Button from './Button'
+import { default as MaterialCommunityIcon} from 'react-native-vector-icons/MaterialCommunityIcons'
+import { default as AntDesignIcon} from 'react-native-vector-icons/AntDesign'
+
+const DeviceInfo = ({name, duration, level}) => {
+  return (
+    <View style={device.container}>
+        <View style={device.title}>
+            <MaterialCommunityIcon style={device.title.bold} name="water-pump" size={25}/>
+            <Text style={device.title.bold}>{name}</Text>
+        </View>
+        <Text style={device.item}>Thời gian: {duration} phút</Text>
+        <Text style={device.item}>Cường độ: {level}</Text>
+        <Button 
+            icon={<AntDesignIcon name="edit" size={15} color="#ffffff"/>}
+            textContent="Chỉnh sửa"
+            bg="#fde047"
+            borderColor="#facc15"
+        />
+
+
+    </View>
+  )
+}
+
+const device = StyleSheet.create({
+    container: {
+        borderColor: "#94a3b8",
+        borderWidth: 1,
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingTop: 5,
+        paddingBottom: 5,
+        width: 150,
+        borderRadius: 5,
+    },
+    title: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        bold: {
+            color: "#0284c7",
+            fontWeight: 600
+        }
+    },
+    item: {
+        color: "#64748b"
+    }
+})
+
+export default DeviceInfo
+
