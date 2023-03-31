af418442080a56db65a7b659395959f60d5e8ad0
tanduy222002 <79954594+tanduy222002@users.noreply.github.com>
2023-03-31 16:32:05 +0700

    test

--- FarmGa/components/SensorView.js
+++ FarmGa/components/SensorView.js
@@ -1,18 +1,36 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 import Area from './Area'
 import TemperatureDetail from './TemperatureDetail'
 import HumidityDetail from './HumidityDetail'
 import LightDetail from './LightDetail'
+
+
 const SensorView = ({navigation}) => {
   function gotoSensorDetaiPage(){
       navigation.navigate("SensorDetail")
   }
+  const [Data,SetData] = useState([])
+  
+
+  useEffect(() => {
+    fetch("http://localhost:3000/areas")
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      SetData(data)
+    })
+  }, [])
+  
+  
   return (
+    
     <ScrollView style={{backgroundColor:'#ebf2f6',}} >
     <View style={{flexDirection:"column",}}>
     <Area name="Khu vực I"  >
-        <TemperatureDetail name="nhiệt độ" level={35} safetyLevel={30} onPress={gotoSensorDetaiPage}/>
+        <TemperatureDetail name="nhiệt độ" level={35} safetyLevel={20} onPress={gotoSensorDetaiPage}/>
         <HumidityDetail name="độ ẩm" level={25} safetyLevel={80} onPress={gotoSensorDetaiPage}/>
         <LightDetail name="ánh sáng" level={25} safetyLevel={80} onPress={gotoSensorDetaiPage}/>
     </Area>
